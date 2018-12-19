b73bf866b4c449fb7e4deb7b5ce9e81a491655df
willibaur <williambautista@gmail.com>
2018-12-19 21:52:00 +0000

    Add editing functionality for band's description

--- app/routes/bands/band/details.js
+++ app/routes/bands/band/details.js
@@ -1,4 +1,17 @@
 import Route from '@ember/routing/route';
 
 export default Route.extend({
+  actions: {
+    willTransition(transition) {
+      if (this.controller.isEditing) {
+        let leave = window.confirm('Are you sure?');
+
+        if (!leave) {
+          transition.abort();
+        } else {
+          this.controller.toggleProperty('isEditing');
+        }
+      }
+    },
+  },
 });
