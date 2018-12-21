535e499855d3cd9f267ba750adb7310dc7bf239b
willibaur <williambautista@gmail.com>
2018-12-21 08:47:39 +0000

    Add Mirage for Acceptance tests

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -1,5 +1,5 @@
 import DS from 'ember-data';
 
 export default DS.JSONAPIAdapter.extend({
-  host: 'http://json-api.rockandrollwithemberjs.com'
+  // host: 'http://json-api.rockandrollwithemberjs.com'
 });
