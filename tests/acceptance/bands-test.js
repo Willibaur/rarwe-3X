cbb46fc07a3ee9bdca6bcc2093a86b872ab150dc
willibaur <williambautista@gmail.com>
2018-12-22 13:36:58 +0000

    Update Mirage for post/

--- tests/acceptance/bands-test.js
+++ tests/acceptance/bands-test.js
@@ -1,5 +1,5 @@
 import { module, test } from 'qunit';
-import { visit } from '@ember/test-helpers';
+import { visit, click, fillIn } from '@ember/test-helpers';
 import { setupApplicationTest } from 'ember-qunit';
 import setupMirageTest from 'ember-cli-mirage/test-support/setup-mirage';
 
@@ -13,11 +13,25 @@ module('Acceptance | Bands', function(hooks) {
 
     await visit('/');
 
-    let bandLinks = document.querySelectorAll('.rr-band-link');
+    assert.dom('[data-test-rr=band-link]').exists({ count: 2 }, 'All band links are rendered');
+    assert.dom('[data-test-rr=band-list-item]:first-child')
+      .hasText('Radiohead', 'The first band link contains the band name');
+    assert.dom('[data-test-rr=band-list-item]:last-child')
+      .hasText('Long Distance Calling', 'The other band link contains the band name');
+  });
 
-    assert.equal(bandLinks.length, 2, 'All band links are rendered');
-    assert.ok(bandLinks[0].textContent.includes('Radiohead'), 'First band link contains the band name');
-    assert.ok(bandLinks[1].textContent
-      .includes('Long Distance Calling'), 'The other band link contains the band name');
+  test('Create a band', async function(assert) {
+    this.server.create('band', { name: 'Royal Blood' });
+
+    await visit('/');
+    await click('[data-test-rr=new-band-label]');
+    await fillIn('[data-test-rr=new-band-input]', 'Caspian');
+    await click('[data-test-rr=new-band-button]');
+
+    assert.dom('[data-test-rr=band-list-item]').exists({ count: 2 }, 'A new band link is rendered');
+    assert.dom('[data-test-rr=band-list-item]:last-child')
+      .hasText('Caspian', 'The new band link is rendered as the last item');
+    assert.dom('[data-test-rr=songs-nav-item] > .active')
+      .hasText('Songs', 'The Songs tab is active');
   });
 });
