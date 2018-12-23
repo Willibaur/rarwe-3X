9255200533928f4f3e7fd8790390331ef8f5333b
willibaur <williambautista@gmail.com>
2018-12-23 22:21:07 +0000

    Sort songs by rating and title

--- app/controllers/bands/band/songs.js
+++ app/controllers/bands/band/songs.js
@@ -1,11 +1,25 @@
 import Controller from '@ember/controller';
-import { empty } from '@ember/object/computed';
+import { empty, sort } from '@ember/object/computed';
+import { computed } from '@ember/object';
 
 export default Controller.extend({
   isAddingSong: false,
   newSongTitle: '',
-
+  sortBy: 'ratingDesc',
   isAddButtonDisabled: empty('newSongTitle'),
+
+  sortProperties: computed('sortBy', function() {
+    let options = {
+      ratingDesc: ['rating:desc', 'title:asc'],
+      ratingAsc:  ['rating:asc', 'title:asc'],
+      titleDesc:  ['title:desc'],
+      titleAsc:   ['title:asc']
+    };
+
+    return options[this.sortBy];
+  }),
+
+  sortedSongs: sort('model.songs', 'sortProperties'),
 
   actions: {
     addSong() {
